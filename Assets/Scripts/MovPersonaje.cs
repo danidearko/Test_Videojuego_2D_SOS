a9594f220c949f84d8f2761fb99e33b7e1b887ec
danidearko <danielddvf16@gmail.com>
2024-03-22 14:17:24 +0100

    Salto arreglado, raycast aplicado...

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -6,17 +6,22 @@ public class MovPersonaje : MonoBehaviour
 {
 
     public float multiplicador = 5f;
+    public float multiplicadorSalto = 5f;
+
+    private bool puedoSaltar = true;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola Mariano xd");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float movTeclas = Input.GetAxis("Horizontal"); //(a -1f - d 1f)
+    //  float movTeclasY = Input.GetAxis("Vertical"); //(a -1f - d 1f)
 
         // aproximación 1 
        /* transform.position = new Vector 3 (
@@ -27,15 +32,75 @@ public class MovPersonaje : MonoBehaviour
 
         float miDeltaTime = Time.deltaTime;
 
-        Debug.Log(Time.deltaTime);
+        // Debug.Log(Time.deltaTime);
 
+        /*
         transform.Translate(
             movTeclas*(Time.deltaTime*multiplicador),
             0,
             0
-        );
+        );*/
 
-        //Debug.Log(Input.GetAxis("Horizontal"));
+
+
+
+
+        //Movimiento Personaje 
+
+        rb.velocity =  new Vector2(movTeclas*multiplicador, rb.velocity.y);
+
+
+        // Flip <-- LEFT
+         if(Input.GetKeyDown(KeyCode.A)){
+                       // this.GetComponent<SpriteRenderer>().flipX = true;
+                       transform.localScale = new Vector3(-1, 1, 1);
+         
+         }
+
+
+        // Flip --> RIGHT
+         if(Input.GetKeyDown(KeyCode.D)){
+                       // this.GetComponent<SpriteRenderer>().flipX = false;
+                       transform.localScale = new Vector3(1, 1, 1);
+         
+         }
+
+    
         
+
+
+
+
+
+
+        //El mejor salto que haya podido existir en la historia del Videojuego contemporáneo 
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.8f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.magenta);
+
+        if(hit){
+            puedoSaltar = true;
+            Debug.Log(hit.collider.name);
+        }else{
+            puedoSaltar = false;
+        }
+ 
+        if(Input.GetKeyDown(KeyCode.Space) && puedoSaltar ){
+            rb.AddForce(
+                new Vector2(0,multiplicadorSalto),
+                ForceMode2D.Impulse
+          );
+        // puedoSaltar = false;
+        }               
     }
+
+    /*
+    void OnCollisionEnter2D(){
+         puedoSaltar = true;
+         Debug.Log("Colision! OMG");
+    }*/
+
+
+
+
 }
